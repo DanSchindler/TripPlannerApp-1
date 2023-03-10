bd50179138842345741ecfcb5047663f531635fd
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-10 17:47:19 +0200

    DB connection func

---
+++ backend/database/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { MONGO_URI } from "../utils/config";
+
+export const connectDB = async () =>{
+    if(!MONGO_URI){
+        console.log('MONGO_URI is undefined in the env file')
+        process.exit(1);
+    }
+    try{
+        await mongoose.connect(MONGO_URI);
+        console.log('Connected successfully to MongoDB');
+    }
+    catch (err){
+     console.log(err.message);
+     process.exit(1);
+    }
+};
