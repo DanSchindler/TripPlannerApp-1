56bbda7f02259c19ec58e9facfe240d8b74e17b9
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-10 22:48:50 +0200

    Basic http exception

--- backend/utils/httpException.ts
+++ backend/utils/httpException.ts
@@ -1,12 +1,11 @@
-export default class HttpException extends Error{
+export default class HttpException extends Error {
     status?: number;
     message: string;
-    error: string|null;
-    constructor(message: string, status: number,error: string|null) {
+    error: string | null;
+    constructor(message: string, status: number, error: string | null) {
         super(message);
         this.status = status;
         this.message = message;
         this.error = error ?? null;
     }
-
 }
