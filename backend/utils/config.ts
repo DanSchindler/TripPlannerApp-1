416b9d7943a9699d0fc1158a4b52adcab0b2b755
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-10 22:49:15 +0200

    syling

--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -1,5 +1,5 @@
-require('dotenv').config();
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 export const PORT = process.env.PORT ?? 5000;
 export const NODE_ENV = process.env.NODE_ENC ?? 'development';
 export const MONGO_URI = process.env.MONGO_URI ?? null;
