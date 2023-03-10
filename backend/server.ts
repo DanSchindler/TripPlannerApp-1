1c490327648819fab5120711dd48234913525361
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-10 21:47:12 +0200

    error handler middleware

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import { PORT } from "./utils/config";
 import { connectDB } from "./database/db";
+import {errorHandler} from "./middleware/errorHandler";
 
 connectDB();
 
 const app = express();
+
 app.use(express.json());
+app.use(errorHandler)
 app.listen(PORT,() => console.log('Server is listening on port: ' + PORT));
