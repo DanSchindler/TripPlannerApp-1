299be2351795b07e0c433eef0ae93a08dde16507
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-10 18:36:41 +0200

    DB connection phase

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,9 @@
 import * as express from 'express';
 import { PORT } from "./utils/config";
+import { connectDB } from "./database/db";
+
+connectDB();
 
 const app = express();
 app.use(express.json());
-app.listen(PORT,() => console.log('server is listening on port: ' + PORT));
+app.listen(PORT,() => console.log('Server is listening on port: ' + PORT));
