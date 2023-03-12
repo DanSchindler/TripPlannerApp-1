6f5454d47fb6f6a70c79cf240b8cc7e9cc741b41
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-12 21:44:55 +0200

    place holder

---
+++ backend/controllers/postsController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+export const getFeedPosts = async (req: Request, res: Response) => {};
+export const getUserFeed = async (req: Request, res: Response) => {};
+export const getUserPosts = async (req: Request, res: Response) => {};
+export const likePost = async (req: Request, res: Response) => {};
+export const createRoute = async (req: Request, res: Response) => {};
+export const createLocation = async (req: Request, res: Response) => {};
