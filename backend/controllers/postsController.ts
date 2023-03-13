6288431b7830bb34b97fcaf5fc8917027fae4b64
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-13 12:02:08 +0200

    reaching endpoints

--- backend/controllers/postsController.ts
+++ backend/controllers/postsController.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from 'express';
-export const getFeedPosts = async (req: Request, res: Response) => {};
-export const getUserFeed = async (req: Request, res: Response) => {};
-export const getUserPosts = async (req: Request, res: Response) => {};
-export const likePost = async (req: Request, res: Response) => {};
-export const createRoute = async (req: Request, res: Response) => {};
-export const createLocation = async (req: Request, res: Response) => {};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getFeedPosts = async (req: Request, res: Response) => {
+    console.log('reached to getFeetPosts');
+};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getUserFeed = async (req: Request, res: Response) => {
+    console.log('reached to getUserFeed');
+};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getUserPosts = async (req: Request, res: Response) => {
+    console.log('reached to getUserPosts');
+};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const likePost = async (req: Request, res: Response) => {
+    console.log('reached to likePost');
+};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const createRoute = async (req: Request, res: Response) => {
+    console.log('reached to createRoute');
+};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const createLocation = async (req: Request, res: Response) => {
+    console.log('reached to createLocation');
+};
