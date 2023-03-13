65f11408168fe3dc82091b05bf7bf354092ed8fa
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-13 12:02:38 +0200

    add posts routes

--- backend/routes/apiRoutes.ts
+++ backend/routes/apiRoutes.ts
@@ -1,8 +1,10 @@
 import userRoutes from './api/userAuthRoutes';
 import { RequestHandler, Router } from 'express';
+import postRouter from './api/postRouter';
 
 const apiRoutes = Router();
 
 apiRoutes.use('/users', userRoutes as RequestHandler);
+apiRoutes.use('/posts', postRouter as RequestHandler);
 
 export default apiRoutes;
