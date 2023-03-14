89bc3478f4aea48abf6ab3efe58766e2aaf2bfc3
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-14 12:38:06 +0200

    lint-fix

--- backend/routes/apiRoutes.ts
+++ backend/routes/apiRoutes.ts
@@ -1,11 +1,8 @@
 import userRoutes from './api/userAuthRoutes';
 import { RequestHandler, Router } from 'express';
-import postRouter from './api/postRouter';
-import { TryCatchMiddleware } from '../middleware/TryCatchMiddleware';
 
 const apiRoutes = Router();
 
 apiRoutes.use('/users', userRoutes as RequestHandler);
-apiRoutes.use('/posts', postRouter as RequestHandler);
 
 export default apiRoutes;
