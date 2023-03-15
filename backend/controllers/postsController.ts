9503ee55610f3c3ffec9a14b4bf77803db09c800
RavidYael <ravidyael.githubuser@gmail.com>
2023-03-15 19:23:22 +0200

    -

--- backend/controllers/postsController.ts
+++ backend/controllers/postsController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { PostType } from '../types/postType';
 import { LocationType } from '../types/locationType';
 import { uploadLocation, uploadPost } from '../logic/posts/posts/postCreator';
+// import {POSTS_IN_PAGE} from "../utils/config";
+// import {supplyPosts} from "../logic/posts/posts/postsSupplier";
 
 export function reachedController(req: Request, res: Response, next: NextFunction) {
     console.log('reached PostsController');
@@ -9,13 +11,25 @@ export function reachedController(req: Request, res: Response, next: NextFunctio
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getFeedPosts = async (req: Request, res: Response) => {
-    console.log('reached to getFeetPosts');
-};
+// export const getAllPosts = async (req: Request, res: Response) => {
+//     const allPosts =
+// };
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getUserFeed = async (req: Request, res: Response) => {
-    console.log('reached to getUserFeed');
-};
+// export const getUserFeed = async (req: Request, res: Response) => {
+//     let pageInd = req.query.page || 1;
+//     let pageSize = req.query.pageSize || POSTS_IN_PAGE;
+//     /*
+//
+//     has to fix conversion between string to numbers, then has to think what do I send, how to handle the
+//     data from the posts? what to return? whos in charge of querying the exact data?
+//
+//     In addition, has to fix the types -
+//     it may be helpful in manner of making sense to myself of what the data and what should I return.
+//
+//      */
+//     //const posts = supplyPosts(,pageSize);
+//
+// };
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getUserPosts = async (req: Request, res: Response) => {
     console.log('reached to getUserPosts');
